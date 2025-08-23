0ca206814ae626ec7baf06037816ea0ab6782c65
qyangcv <2275779228@qq.com>
2025-08-23 22:40:32 +0800

    implement dca (dynamic channels aligner)

--- patch_embed_align/view_configs.py
+++
@@ -1,8 +0,0 @@
-import os
-from omegaconf import OmegaConf
-
-os.chdir('/data1/yangquan/DOFA-pytorch')
-cfg = OmegaConf.load('src/configs/model/dca_seg.yaml')
-
-print(f"Teacher: {cfg.pretrained_teacher_path}")
-print(f"Student: {cfg.pretrained_student_path}")
